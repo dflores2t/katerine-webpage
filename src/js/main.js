a323f49d643ece54d54fe05d7d1344b6d0ff6706
Daniel Flores <dflores@gmail.com>
2021-06-30 13:16:37 -0600

    PROYECTO KHATERINE FASIHON - INICIO

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+import "../js/vendor/modernizr-3.11.2.min.js";
+import "../js/plugins";
+import 'bootstrap/dist/js/bootstrap.bundle'
+import "photoswipe/dist/photoswipe";
+import "photoswipe/dist/photoswipe-ui-default";
+
+
+import * as urlImage from "../img/front/llogo.svg";
+
+var canvas = document.getElementById("canvas");
+var lineFooter = document.getElementById("lineFooter");
+var lineBotton = document.getElementById("lineBotton");
+const ctx = canvas.getContext("2d");
+const image = new Image(); // Using optional size for image
+image.onload = drawImage; // Draw when image has loaded
+
+// Load an image of intrinsic size 300x227 in CSS pixels
+image.src = urlImage.default;
+
+function drawImage() {
+  // Use the intrinsic size of image in CSS pixels for the canvas element
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight;
+
+  // using the element's width and height properties - lets draw one
+  // on top in the corner:
+  ctx.drawImage(this, 0, 0, this.width, this.height);
+  if (lineFooter.getContext) {
+    var l = lineFooter.getContext("2d");
+    l.beginPath();
+    l.moveTo(0, 20);
+    l.lineTo(300, 20);
+    // set line grosor
+    l.lineWidth = 5;
+    // set color to line
+    l.strokeStyle = "#ff0e8d";
+    l.stroke();
+  }
+  if (lineBotton.getContext) {
+    var line = lineBotton.getContext("2d");
+    line.font = "12px Comic Sans MS";
+    line.fillStyle = "white";
+    line.textAlign = "rigth";
+    line.fillText("CopyRight@ Propositivapr 2021", 70, 30);
+  }
+}
