a0a1b88cc06a510e442557d022374dc70ac0d891
Daniel Flores <dflores2t@gmail.com>
2022-01-25 14:42:54 -0600

    restructuracion del sito, cambio de colores, texto, modal

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,14 @@ import "bootstrap/dist/js/bootstrap.bundle";
 import "photoswipe/dist/photoswipe";
 import "photoswipe/dist/photoswipe-ui-default";
 
-import * as urlImage from "../img/front/llogo.svg";
-
-var canvas = document.getElementById("canvas");
+// var canvas = document.querySelector("canvas");
 var lineFooter = document.getElementById("lineFooter");
 var lineBotton = document.getElementById("lineBotton");
-const ctx = canvas.getContext("2d");
 const image = new Image(); // Using optional size for image
-image.onload = drawImage; // Draw when image has loaded
-
-// Load an image of intrinsic size 300x227 in CSS pixels
-image.src = urlImage.default;
+image.onload = drawImage(); // Draw when image has loade
 
 function drawImage() {
   // Use the intrinsic size of image in CSS pixels for the canvas element
-  canvas.width = this.naturalWidth;
-  canvas.height = this.naturalHeight;
-
-  // using the element's width and height properties - lets draw one
-  // on top in the corner:
-  ctx.drawImage(this, 0, 0, this.width, this.height);
   if (lineFooter.getContext) {
     var l = lineFooter.getContext("2d");
     l.beginPath();
@@ -38,8 +26,8 @@ function drawImage() {
   }
   if (lineBotton.getContext) {
     var line = lineBotton.getContext("2d");
-    line.font = "12px Comic Sans MS";
-    line.fillStyle = "white";
+    line.font = "13px Comic Sans MS";
+    line.fillStyle = "black";
     line.textAlign = "rigth";
     line.fillText("CopyRight@ Propositivapr 2021", 70, 30);
   }
@@ -52,10 +40,10 @@ const menu = {
   "Arte & Moda León": "arteYmodaLeon",
   "Nicaragua Diseña Edición SJS": "ndesings",
   "Edición 2021": "n2021",
-  "Edición 2022": "n2022",
+  "Edición 2020": "n2022",
   "Edición Granada": "ngranada",
-  "Coleccion Summer Time": "summerTime",
-  "Coleccion Metamorfosis": "Metamorfosis",
+  "Colección Summer Time": "summerTime",
+  "Colección Metamorfosis": "Metamorfosis",
   "Arte, Moda, Diseño y Muralismo": "esteli",
   "Miss Teens Nicaragua": "mteens2021",
 };
